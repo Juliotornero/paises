b3c2f2215a212ab66a2d76433a0bbc14be086244
Julio <juliotornero8@gmail.com>
2024-10-26 22:10:12 -0500

    terminado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -58,7 +58,7 @@ const SearchBar = ({ isFocused, setIsFocused }) => {
 
   return (
     <div className="sticky top-0 left-0 right-0 z-50 w-full max-w-[290px] md:max-w-[400px] lg:max-w-[600px] mb-6 search-container">
-      <div className="flex justify-between items-center rounded-full py-1 pl-6 pr-4 w-full shadow-xl bg-white">
+      <div className="flex justify-between items-center rounded-full py-1 pl-4 pr-2 w-full shadow-xl bg-white">
         <div className="flex flex-col flex-grow">
           <label className="text-gray-400 font-medium">País</label>
           <input
@@ -67,38 +67,38 @@ const SearchBar = ({ isFocused, setIsFocused }) => {
             value={searchTerm}
             onChange={handleSearchChange}
             onFocus={() => setIsFocused(true)}
-            className="focus:outline-none focus:ring-0 text-gray-400 placeholder:text-xs border-b-2 border-transparent focus:border-blue-400"
+            className="focus:outline-none focus:ring-0 text-gray-400 placeholder:text-xs border-b-2 border-transparent focus:border-blue-400 text-sm sm:text-base"
           />
         </div>
         <button
           onClick={() => setIsFocused(false)}
-          className="w-28 h-8 bg-blue-500 text-white rounded-full px-4 py-2 ml-2 flex justify-center items-center"
+          className="w-20 h-8 sm:w-28 bg-blue-500 text-white rounded-full px-2 sm:px-4 py-2 ml-1 sm:ml-2 flex justify-center items-center text-xs sm:text-sm"
           aria-label="Cerrar búsqueda"
         >
-          <i className="fas fa-search mr-2 hidden md:inline"></i>
-          <span className="md:inline hidden">Buscar</span>
-          <i className="md:hidden fas fa-search"></i>
+          <i className="fas fa-search mr-1 hidden sm:inline"></i>
+          <span className="sm:inline hidden">Buscar</span>
+          <i className="sm:hidden fas fa-search"></i>
         </button>
       </div>
 
       {isFocused && (
         <div
           ref={modalRef}
-          className="absolute top-full left-0 mt-2 w-3/4 bg-white p-4 rounded-lg shadow-lg z-10"
+          className="absolute top-full left-0 mt-2 w-full max-w-[280px] md:w-3/4 bg-white p-4 rounded-lg shadow-lg z-10"
         >
           <div className="flex justify-between items-center mb-4">
             <p className="font-semibold text-gray-500">
               Filtrar por continentes
             </p>
             <button
-              onClick={clearSearch} // Llama a la función clearSearch
+              onClick={clearSearch}
               className="text-blue-500 font-bold px-4 py-2 rounded-md"
             >
               Limpiar
             </button>
           </div>
 
-          <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 place-items-center">
+          <div className="grid grid-cols-2 gap-2  md:grid-cols-2 place-items-center">
             {[
               "Europe",
               "North America",
@@ -109,7 +109,7 @@ const SearchBar = ({ isFocused, setIsFocused }) => {
             ].map((continent) => (
               <div
                 key={continent}
-                className={`flex flex-col items-center justify-center border rounded-lg p-1 w-24 h-24 cursor-pointer transition-all duration-200 ${
+                className={`flex flex-col items-center justify-center border rounded-lg p-1 w-20 h-20 sm:w-24 sm:h-24 cursor-pointer transition-all duration-200 ${
                   selectedContinents.has(continent)
                     ? "border-blue-500 bg-blue-50"
                     : "border-transparent"
@@ -119,10 +119,9 @@ const SearchBar = ({ isFocused, setIsFocused }) => {
                 <img
                   src={`/${continent}.svg`}
                   alt={`Icono de ${continent}`}
-                  className="w-10 h-10 mb-2" 
+                  className="w-8 h-8 sm:w-10 sm:h-10 mb-1"
                 />
-                <p className="text-center text-xs">{continent}</p>{" "}
-                
+                <p className="text-center text-xs">{continent}</p>
               </div>
             ))}
           </div>
